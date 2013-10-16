933955504dad83db78f6454d9645e73123bfce04
Erik Sjöstrand <sjostrand.erik@gmail.com>
2013-10-16 12:56:50 +0200

    first commit

---
+++ emacs.d/init.el
@@ -0,0 +1,15 @@
+(defvar my-packages '(ace-jump-mode
+                      auctex
+                      auto-complete
+                      bookmark+
+                      deft
+                      expand-region
+                      flycheck
+                      helm helm-spotify
+                      lua-mode
+                      multiple-cursors
+                      nrepl paredit slime
+                      yasnippet
+                      zenburn-theme)
+  "List of packages to install at launch")
+
