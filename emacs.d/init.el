03142f27b0d099992170ad0cca8e36b2da201eec
Erik Sjöstrand <sjostrand.erik@gmail.com>
2014-06-03 17:11:25 +0200

    Changes to my .org workflow.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,3 +4,15 @@
 (require 'ob-tangle)
 
 (org-babel-load-file "~/.emacs.d/evilinit.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
