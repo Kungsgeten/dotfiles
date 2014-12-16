ab10847e320c8a34b978e67f3a732ade5547f8c2
Erik Sjöstrand <sjostrand.erik@gmail.com>
2014-12-16 15:18:59 +0100

    Dired shortcuts, new § prefixes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,7 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(coffee-tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
